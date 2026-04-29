0877c68202e1208281d07115b5873b176bc7a02e
Abdelrahman-abdelnaser <abdelnaserabdelrahman74@gmail.com>
2026-04-29 16:06:08 +0300

    fix: correct button ID and improve weather data fetching logic

--- weather.js
+++ weather.js
@@ -1,33 +1,44 @@
 const apiKey = "33f91fb51e8749ccb7a194051260204";
-let cityName;
 let myInput = document.querySelector("#search-input");
 let mycity = document.querySelector(".city-name");
-let mySearchBtn = document.querySelector("#search-botton");
+let mySearchBtn = document.querySelector("#search-button");
 let myTemprature = document.querySelector(".temprature");
 let myDate = document.querySelector(".date");
-let year = new Date().getFullYear();
-let mm = new Date().getMonth() + 1;
-let day = new Date().getDate();
 let myHumidity = document.querySelector(".humidity");
 
-myDate.textContent = `${day}/${mm}/${year}`;
+myDate.textContent = date();
 
-mySearchBtn.addEventListener("click", () => {
-  cityName = myInput.value;
-  mycity.textContent = myInput.value;
-  weatherData().then((data) => {
-    myHumidity.textContent = data.current.humidity;
-    myTemprature.textContent = data.current.temp_c + " " + "°C";
-  });
-});
+function date() {
+  let year = new Date().getFullYear();
+  let month = new Date().getMonth() + 1;
+  let day = new Date().getDate();
+  return `${day}/${month}/${year}`;
+}
 
-async function weatherData() {
+async function getData(city) {
   try {
-    let res = await fetch(`https://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${cityName}`);
+    let res = await fetch(`https://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${city}`);
+    if (!res.ok) throw new Error("City Not Found");
     let data = await res.json();
 
     return data;
   } catch (err) {
-    console.log(err);
+    alert(err.message);
+    return null;
   }
 }
+async function displayWeather(data) {
+  myHumidity.textContent = data.current.humidity;
+  myTemprature.textContent = data.current.temp_c + " °C";
+}
+
+mySearchBtn.addEventListener("click", async () => {
+  let city = myInput.value;
+  if (!city) {
+    alert("Enter city name");
+    return;
+  }
+  mycity.textContent = city;
+  let data = await getData(city);
+  displayWeather(data);
+});
